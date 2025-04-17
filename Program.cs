4f9e7191e1ff2ec8fcab6d9bbc56a237354af85a
Rimz516 <ch0bits05863@gmail.com>
2025-04-17 14:50:03 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Program
+{    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo key;
+
+            int[,] map = new int[50, 50];
+            int x = 0;
+            int y = 0;
+
+            while (true)
+            {
+                
+                Console.CursorVisible = false;
+
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine("▣");                
+
+                key = Console.ReadKey(true);
+
+                Console.SetCursorPosition(x, y);
+                Console.Write("  ");
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        if(x > 1)
+                        x--;
+                        break;
+                    case ConsoleKey.RightArrow:  
+                        if(x < 49)
+                        x++;
+                        break;
+                    case ConsoleKey.UpArrow:  
+                        if(y > 1)
+                        y--;
+                        break;
+                    case ConsoleKey.DownArrow:   
+                        if(y < 49)
+                        y++;
+                        break;
+                }                
+            }           
+        }
+    }
+}
